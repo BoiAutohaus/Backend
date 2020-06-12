d9dde7d4ac127b14399b2c530b0b843ae332d86d
tanktop7 <65771895+tanktop7@users.noreply.github.com>
2020-06-12 16:17:38 +0200

    rein da

--- services/register.js
+++ services/register.js
@@ -31,7 +31,7 @@ serviceRouter.post("/login", function(request, response){
             var success = "Successfully logged in";
             helper.log(success);
             response.status(200).json(helper.jsonMsgOK(success));
-            window.alert("angemeldet")
+           
         }
         else{
             helper.log("Failed to login! Passwort oder eMail falsch!");
@@ -99,7 +99,7 @@ serviceRouter.post("/register", function(request, response) {
     try {
         var result = registerDao.create(request.body.mail,request.body.vorname,request.body.nachname,request.body.strasse, request.body.hausnummer, request.body.ort, request.body.plz, request.body.passwort);
         helper.log("Service Register: Record inserted");
-        response.status(200).json({email:'Erfolgreich ' , password: 'Registriert'});
+        response.status(200).json({ email:'erfolgreich ' , password: 'registriert'});
 
     } catch (ex) {
         helper.logError("Service Register: Error creating new record. Exception occured: " + ex.message);
