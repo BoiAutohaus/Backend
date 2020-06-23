64647c2fc401b218b877116e1d7d49aa6262d34c
schrotle <65763511+schrotle@users.noreply.github.com>
2020-06-23 17:19:55 +0200

    changes

--- services/search.js
+++ services/search.js
@@ -1,12 +1,12 @@
 const helper = require("../helper.js");
-const RegisterDao = require("../dao/registerDao.js");
+const SearchDao = require("../dao/searchDao.js");
 const express = require("express");
 var serviceRouter = express.Router();
 
 serviceRouter.get("/register/gib/:mail", function(request, response) {
     helper.log("Service Register: Client requested one record, eMail=" + request.params.mail);
 
-    const registerDao = new RegisterDao(request.app.locals.dbConnection);
+    const searchDao = new SearchDao(request.app.locals.dbConnection);
     try {
         var result = registerDao.loadByMail(request.params.mail);
         helper.log("Service Register: Record loaded");
@@ -16,6 +16,13 @@ serviceRouter.get("/register/gib/:mail", function(request, response) {
         helper.logError("Service Register: Error loading record by id. Exception occured: " + ex.message);
         response.status(400).json(helper.jsonMsgError(ex.message));
     }
+});
+
+serviceRouter.get("/auto?marke=?&modell=?&erstzulassung=?&kilometer=?&region=?&adresse=?&preis=?&kraftstoffart=?", function(request,response){
+    continue
+    //NOT SURE IF PARAMETERS ARE CORRECT AND IF THIS EVEN WORKS LIKE THIS
+    //CODE NEEDS TO BE HERE
+    // REST OF THE CODE IS PROBABLY NOT NEEDED
 });
 
 serviceRouter.post("/login", function(request, response){
